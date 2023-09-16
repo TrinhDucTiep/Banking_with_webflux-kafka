0843ff46d1cce49bf34a06a531379f4424babc72
TrinhDucTiep <nhungthisope123@gmail.com>
2023-09-16 15:11:57 +0700

    write migration script with liquibase

---
+++ profileservice/src/main/resources/db/scripts/sql/table-profile-init.sql
@@ -0,0 +1,3 @@
+INSERT INTO profile (id, email, status, name, role) VALUES
+(1, 'tieptd@gmail.com', 'ACTIVE', 'Trịnh Đức Tiệp', 'DEVELOPER'),
+(2, 'haponake123@gmail.com', 'ACTIVE', 'Phượng Trần', 'CUSTOMER');
